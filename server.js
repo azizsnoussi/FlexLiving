f29284f578780a3eac19dfdc1a5790e43b457102
mohamed aziz snoussi <63208771+azizsnoussi@users.noreply.github.com>
2025-08-11 22:46:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const serverless = require('serverless-http');
 const reviewRoutes = require('./routes/reviewRoutes');
 
 const app = express();
 app.use(express.json());
 
-// Routes
 app.use('/api/reviews', reviewRoutes);
 
-const PORT = process.env.PORT || 4000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 4000;
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
 
+// Export for Vercel
+module.exports = app;
+module.exports.handler = serverless(app);
