f4248b29dcf115a1315478ca7b3b3301d6a4234c
azizsnoussi <63208771+azizsnoussi@users.noreply.github.com>
2025-08-12 00:30:14 +0100

    final url fix deploy

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const cors = require('cors');      // Add this
 const reviewRoutes = require('./routes/reviewRoutes');
 
 const app = express();
+
+app.use(cors({
+  origin: 'https://flexlivingfront.vercel.app'
+}));
+
 app.use(express.json());
 
 app.use('/api/reviews', reviewRoutes);
@@ -9,7 +15,6 @@ app.use('/api/reviews', reviewRoutes);
 app.get('/', (req, res) => res.send('API is running!'));
 
 const PORT = process.env.PORT || 4000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
