2f79d69da7c093b6c7883a69aed676e6abe521d9
azizsnoussi <63208771+azizsnoussi@users.noreply.github.com>
2025-08-11 22:55:15 +0100

    fix redeploy

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const reviewRoutes = require('./routes/reviewRoutes');
 const app = express();
 app.use(express.json());
 
-// Routes
 app.use('/reviews', reviewRoutes);
 
-// Export as serverless function
 module.exports = app;
 module.exports.handler = serverless(app);
